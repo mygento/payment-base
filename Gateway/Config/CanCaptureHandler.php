34620a40b7f9308ca6e7dadd5d9f3abec438f9db
Nikita <luckyraul@users.noreply.github.com>
2017-10-11 16:04:08 +0300

    Fix doc

--- Gateway/Config/CanCaptureHandler.php
+++ Gateway/Config/CanCaptureHandler.php
@@ -3,7 +3,7 @@
 /**
  * @author Mygento Team
  * @copyright Copyright 2017 Mygento (https://www.mygento.ru)
- * @package Mygento_Cloudpayments
+ * @package Mygento_Payment
  */
 
 namespace Mygento\Payment\Gateway\Config;
